4635596154116929b39674bbaa4c6525a8d02db2
yuankang134 <373346037@qq.com>
2023-04-13 09:31:39 +0800

    stop脚本优化

--- server_stop.sh
+++ server_stop.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-#read -p "输入 你要kill的端口号: " a
+echo -e "sql languge server will be stopped"
 echo -e "kill server port 3033"
-for SERVER_PID in `lsof -i:3033 | awk '{print $2}' | grep -v \'PID\'`;
+for SERVER_PID in `netstat -nlp | grep :3033 | awk '{print $7}' | awk -F"/" '{ print $1 }' | grep -v \'PID\'`;
  do 
    echo -e "language server process id: " ${SERVER_PID}; 
-   kill -9 $SERVER_PID;
+   kill -q $SERVER_PID;
 done
-
 echo -e "language server is stop"
 
+sleep 2s
+cur_pid=$$
+base_name=$(basename $BASH_SOURCE)
+echo -e "check sql-language-server process"
+for SLS_PID in `ps aux | grep 'src/server/server.ts' | egrep -v "grep|$base_name" | awk '{print $2}' | sed "/$cur_pid/d"`
+ do
+   echo -e "sql-language-server process id: " ${SLS_PID};
+   kill -q $SLS_PID;
+done
+
+sleep 2s
+echo -e "sql-language-server stop completion"
