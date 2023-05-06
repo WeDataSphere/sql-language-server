fcca83ec313eeb29cfd4c715ce5e1fb9b3368743
yuankang134 <373346037@qq.com>
2023-05-06 17:15:38 +0800

    shell脚本优化

--- server_stop.sh
+++ server_stop.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 echo -e "sql languge server will be stopped"
+
+base_path=$(cd `dirname $0`;pwd)
+echo -e "current pwd: ${base_path}"
+
 # 读取配置文件
 config=$(cat env.json)
 
 # 解析 JSON 数据
 log_file=$(echo "${config}" | jq -r '.log_file')
-server_port=$(echo "${config}" | jq -r '.server_port')
+record_path=$(dirname $log_file)/language-server-kill-record.log
+# server_port=$(echo "${config}" | jq -r '.server_port')
 
-echo -e "kill server port ${server_port}"
-for SERVER_PID in `netstat -nlp | grep :${server_port} | awk '{print $7}' | awk -F"/" '{ print $1 }' | grep -v \'PID\'`;
- do 
-   echo -e "language server process id: " ${SERVER_PID}; 
-   kill -9 $SERVER_PID;
-done
-echo -e "language server is stop"
+# echo -e "kill server port ${server_port}"
+# if [ -z ${server_port} ]; then
+#    echo -e "server port is empty, server will exit"
+#    exit 1
+# fi
 
-sleep 2s
-cur_pid=$$
-base_name=$(basename $BASH_SOURCE)
+# for SERVER_PID in `netstat -nlp | grep :${server_port} | awk '{print $7}' | awk -F"/" '{ print $1 }' | grep -v \'PID\'`;
+#  do 
+#    echo -e "language server process id: " ${SERVER_PID}; 
+#    kill -9 $SERVER_PID;
+# done
+# echo -e "language server is stop"
+
+current_time=$(date "+%Y.%m.%d-%H:%M:%S")
+
 echo -e "check sql-language-server process"
-for SLS_PID in `ps aux | grep 'src/server/server.ts' | egrep -v "grep|$base_name" | awk '{print $2}' | sed "/$cur_pid/d"`
+for SLS_PID in `ps aux | grep ${base_path} | grep nodejs/bin/node | egrep -v "grep" | awk '{print $2}'`
  do
-   echo -e "sql-language-server process id: " ${SLS_PID};
-   kill -9 $SLS_PID;
+   echo -e "[${current_time}][sql-language-server] kill process id: ${SLS_PID}" |tee -a ${record_path}
+   kill -15 $SLS_PID;
 done
 
-sleep 2s
 echo -e "sql-language-server stop completion"
